7cded321f5fb9c2d6a5d2e16841fbcf692c36547
JakeLandowski <jakelandowski@gmail.com>
2018-01-30 20:01:04 -0800

    Finished function invocations in validate.php

--- model/validate.php
+++ model/validate.php
@@ -1,9 +1,8 @@
 <?php
-
 /**
-* Validate a color
-*   
-* @param String color
+ * Validate a color
+ *   
+ * @param String color
 * @return boolean
 */
 function validColor($color)
@@ -21,4 +20,21 @@ function validColor($color)
 function validString($str)
 {
     return !empty(str) && ctype_alpha(str);
+}
+
+$errors = [];
+
+if(!validColor($color))
+{
+    $errors['color'] = 'Please enter a valid color.';
+}
+
+if(!validString($name))
+{
+    $errors['name'] = 'Please enter a valid pet name.';
+}
+
+if(!validString($type))
+{
+    $errors['type'] = 'Please enter a valid pet type.';
 }
