3d45107e2255c821ccb6be64d4a4c0ecb37118fc
JakeLandowski <jakelandowski@gmail.com>
2018-01-30 19:54:20 -0800

    Finished validString() function

--- model/validate.php
+++ model/validate.php
@@ -11,3 +11,14 @@ function validColor($color)
     global $f3;
     return in_array($color, $f3->get('colors'));    
 }
+
+/**
+ *  Validate a string
+ * 
+ *  @param String str
+ *  @return boolean
+ */
+function validString($str)
+{
+    return !empty(str) && ctype_alpha(str);
+}
